6bf084e51f507c71fa96a3f91136f1aaf3e27ad1
prumat4 <didukhmaksym4@gmail.com>
2023-07-16 13:12:41 +0300

    add message sending between client and server

--- client/client.cpp
+++ client/client.cpp
@@ -1,42 +1,41 @@
 #include <iostream>
 #include <boost/asio.hpp>
-#include <array>
+#include <string>
 
 using boost::asio::ip::tcp;
 
 int main() {
-    try{
+    try {
         boost::asio::io_context io_context;
 
-        tcp::resolver resolver (io_context);
+        tcp::resolver resolver(io_context);
         boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "2222");
 
-        tcp::socket socket (io_context);
+        tcp::socket socket(io_context);
 
-        // establishes a connection between a socket and one of the endpoints in a sequence of resolved endpoints.
         boost::asio::connect(socket, endpoints);
 
-        for(;;) {
-            // listen for message
-            std::array<char, 128> buf;
-            boost::system::error_code error;
-
-            // reads data from a socket into a buffer and returns the number of bytes read
-            size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-            // end of file
-            // In network programming, an "end of file" error typically occurs when the connection has been closed or terminated by the remote endpoint.
-            // It indicates that no more data will be received from the socket.
-            if(error == boost::asio::error::eof) {
-                // clean connection cut off
-                break;
-            } else if(error) {
-                throw boost::system::system_error(error);
-            }
-
-            std::cout.write(buf.data(), len);
-        }
-    } catch (std::exception& e){
+        std::string message; 
+        boost::system::error_code error;
+        
+        std::cout << "Give me your message: ";
+        std::getline(std::cin, message);
+        
+        boost::asio::write(socket, boost::asio::buffer(message), error);
+        
+        if (error)
+            throw boost::system::system_error(error);
+        
+        std::string response;
+        response.resize(128);
+        
+        socket.read_some(boost::asio::buffer(response), error);
+        
+        if (error)
+            throw boost::system::system_error(error);
+        
+        std::cout << "Response from server: " << response << std::endl;
+    } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
 
--- server/server.cpp
+++ server/server.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <string>
 
 #define PORT_NUMBER 2222
 
 using boost::asio::ip::tcp;
 
 int main() {
-    try{
-        std::cout << "Accepting connections on port " << PORT_NUMBER << "...\n";
-        
-        // create context for I/O functionality
-        boost::asio::io_context io_context;
-        
-        // create acceptor, which will listen on some socket on specific endpoint 
-        // with io_context and endpoint(IPv4, PORT_NUMBER)
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT_NUMBER));
-        
-        // for while
-        for(;;) {
-        
-            // create a socket which is enpoint for sending a receving data
-            tcp::socket socket(io_context);
-        
-            // accept connection of client on socket
-            // accept() block until client is connected
-            acceptor.accept(socket);
+    try {
+        std::cout << "Accepting connections on port" << PORT_NUMBER << "...\n";
 
-            std::cout << "Client connected! Sending message...\n";
-            std::string message = "Hello, client";
+        boost::asio::io_context io_context;
+
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT_NUMBER));
+
+        for (;;) {
             boost::system::error_code error;
 
-            //write() writes data to a socket using buffer
-            boost::asio::write(socket, boost::asio::buffer(message), error);
+            tcp::socket socket(io_context);
+
+            acceptor.accept(socket);
+
+            std::cout << "Client connected! Waiting for message...\n";
+
+            std::string buf; 
+            buf.resize(128);
+
+            socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error)
+                throw boost::system::system_error(error);
+
+            std::cout << "Received message from client: " << buf << std::endl;
+            boost::asio::write(socket, boost::asio::buffer(buf), error);
         }
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
